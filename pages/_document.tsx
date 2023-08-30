140c2a323769bace479a302ca91ceedbccc66bce
orangeVaper <gtenio+orange@gmail.com>
2023-08-31 00:04:44 +0900

    コードブロックにスクロールバーauto指定

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='font-body'>
+      <body className='break-all font-body'>
         <Main />
         <NextScript />
       </body>
